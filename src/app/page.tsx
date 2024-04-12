41bfe05d8df4929cdd511032e7a9bbc1b5f98964
peetza051 <165657508+peetza051@users.noreply.github.com>
2024-04-12 17:53:10 +0700

    same go next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,23 +19,23 @@ export default function Navbar() {
   <div className="bg-blue-900  box-border h-14 w-200  flex  text-sm font-bold justify-around border-1 border-red-900 ">
     
     <img  src="https://finstable.co.th/images/logo-finstable-white.png" className="flex w-1/10.5 h-7 order--1 my-auto justify-start  border-1 border-white"  /> {/*pr-44*/}
-     <ul className=" flex space-x-4 my-auto">
+     <ul className=" flex p-4 space-x-4 my-auto border-2 border-white ">
       
-      {list1.map((item:any, index:number) => {
-        return (
-          <a key={index} 
-             href={item.srcl} className=" text-white "> {/* href คือการลิ้งค์ item.srcl กับnavbarข้างบน */}
-               {item.name}
-            
-          </a>
-        );
-      })}
+     {list1.map((item:any, index:number) => {
+  return (
+    <a key={index} 
+       href={item.srcl} className=" text-white  hover:text-nav "> {/* href คือการลิ้งค์ item.srcl กับnavbarข้างบน */}
+         {item.name}
+      
+    </a>
+  );
+})}
      </ul>
     
   </div>
   <div className="  ">
     <div className=" colum-van flex  p-60 bg">
-     <h1 className=" text-center text-white text-5xl  mb-8 mui-style-10gnrls border-1 border-red-900 box-border h-16 w-48">ข้อมูลบริษัท</h1>
+     <h1 className=" text-center text-white text-5xl  mb-8 mui-style-10gnrls border-1 border-red-900 box-border h-16 w-44">ข้อมูลบริษัท</h1>
      <b className="mt-2 text-white  border-1 border-red-900 " >ผู้ให้บริการด้านโซลูชั่นบล็อกเชนและศูนย์เทคโนโลยีชั้นนำแห่งเอเชีย</b>
     </div>
   </div>
@@ -47,3 +47,4 @@ export default function Navbar() {
 // export default function detai1() {
 
 // }
+
